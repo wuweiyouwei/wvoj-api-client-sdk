ba0f2a37c8f3af6fc5ac9e705cba589f84f56c9b
wuweiyouwei <2119278527@qq.com>
2024-03-02 18:56:30 +0800

    init sdk

---
+++ src/main/java/com/wv/wvojapiclientsdk/WvojApiClientSdkApplication.java
@@ -0,0 +1,13 @@
+package com.wv.wvojapiclientsdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WvojApiClientSdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WvojApiClientSdkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wv/wvojapiclientsdk/WvojApiClientSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wv.wvojapiclientsdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WvojApiClientSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
